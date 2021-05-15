bc115216649775243f0664dc7b40a8b02f202fe6
JeanMic <jean121421@gmail.com>
2021-05-15 13:42:08 -0300

    first commit

---
+++ src/main/java/com/jean/salestax/SalestaxApplication.java
@@ -0,0 +1,13 @@
+package com.jean.salestax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestaxApplication.class, args);
+	}
+
+}
