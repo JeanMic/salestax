61acb80aa3375368a1d7ae13280518f64b2348ed
Jean <jean121421@gmail.com>
2021-05-16 17:02:53 -0300

    Removing unused dependencies

--- src/main/java/com/jean/salestax/api/dto/PurchaseDTO.java
+++ src/main/java/com/jean/salestax/api/dto/PurchaseDTO.java
@@ -1,16 +1,5 @@
 package com.jean.salestax.api.dto;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-
-import org.h2.util.json.JSONObject;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
