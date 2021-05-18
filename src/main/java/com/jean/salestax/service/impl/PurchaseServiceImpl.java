583e15c4bd36a40200664c2e123bf7e233eb2254
Jean <jean121421@gmail.com>
2021-05-17 21:35:56 -0300

    [Rollback-DB] Service adjusted to use the new product object

--- src/main/java/com/jean/salestax/service/impl/PurchaseServiceImpl.java
+++ src/main/java/com/jean/salestax/service/impl/PurchaseServiceImpl.java
@@ -2,62 +2,44 @@ package com.jean.salestax.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jean.salestax.api.dto.PurchaseDTO;
 import com.jean.salestax.api.dto.PurchaseReceiptDTO;
 import com.jean.salestax.api.dto.PurchaseReceiptItemDTO;
+import com.jean.salestax.factories.ProductFactory;
 import com.jean.salestax.model.entity.Product;
-import com.jean.salestax.model.repository.ProductRepository;
 import com.jean.salestax.service.PurchaseService;
 
 @Service
 public class PurchaseServiceImpl implements PurchaseService
 {
-	private ProductRepository repository;
-	
-	@Autowired
-	public PurchaseServiceImpl(ProductRepository repository) {
-		super();
-		this.repository = repository;
-	}
-
-	@Override
-	public boolean existsById(Long id) {
-		return repository.existsById(id);
-	}
-	
 	@Override
 	public PurchaseReceiptDTO sumary(List<PurchaseDTO> dtos) {
 		
-		List<Product> listProducts = new ArrayList<Product>();
+		ProductFactory productFactory = new ProductFactory();
+		List<Product> products = productFactory.createProduct(dtos);
 		ArrayList<PurchaseReceiptItemDTO> listItens = new ArrayList<>();
 
-		for (PurchaseDTO dto : dtos) {
-			
-			Product product = (repository.findById(dto.getProductId())).get();
-
-			listProducts.add(product);
-			listItens.add(buildPurchaseReceiptItem(dto, product));
+		for (Product product : products) {
+			listItens.add(buildPurchaseReceiptItem(product));
 		}
 
-		Double tax = Calculator.calculateTaxs(listProducts);
+		Double tax = Calculator.calculateTaxs(products);
 
-		PurchaseReceiptDTO result = buildPurchaseReceipt(tax, listProducts, listItens);
+		PurchaseReceiptDTO result = buildPurchaseReceipt(tax, products, listItens);
 		return result;
 		
 	}
 	
-	private PurchaseReceiptItemDTO buildPurchaseReceiptItem(PurchaseDTO dto, Product product) {
+	private PurchaseReceiptItemDTO buildPurchaseReceiptItem(Product product) {
 		
 		Double calculatedPrice = Calculator.calculateAmountOfProduct(product);
 		
 		return PurchaseReceiptItemDTO.builder()
-				.quantity(dto.getQuantity())
-				.description(product.getDescription())
+				.quantity(product.getQuantity())
+				.description(product.getName())
 				.calculatedPrice(calculatedPrice)
 				.build();
 	}
